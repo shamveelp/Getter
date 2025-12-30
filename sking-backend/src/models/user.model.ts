16b5a5881748a8c7d74ed963149160e5e3164c5f
Shamveel P <shamveelp007@gmail.com>
2025-12-31 01:25:40 +0530

    feat(model) - backend model of the user updated

---
+++ sking-backend/src/models/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document, Model, Types } from "mongoose";
+import { ObjectId } from "mongodb";
+
+export interface IUser extends Document {
+  _id: ObjectId;
+  username: string;
+  email: string;
+  password: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: false },
+    isActive: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
+
+export const UserModel: Model<IUser> = mongoose.model<IUser>(
+  "User",
+  UserSchema
+);
