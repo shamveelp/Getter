f348f7d53ce801d22fe603e1d05bd728af0dcc89
Shamveel P <shamveelp007@gmail.com>
2026-03-22 14:55:47 +0530

    feat(trigger) - triggering the backend ci/cd

--- getter-frontend/src/lib/axios.ts
+++ getter-frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'sonner';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001',
     withCredentials: true, // For cookies (refresh token)
     timeout: 10000,
     headers: {
